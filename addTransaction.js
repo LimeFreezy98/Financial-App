484dbe68998621b79292793000c91b5b4fc5d9ed
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-10-13 10:56:48 -0600

    adding function to able calculate transaction type and ammount would calculate together with total income, total expenses and Remaining Balance in dashboard page

---
+++ addTransaction.js
@@ -0,0 +1,53 @@
+//  addTansaction.js
+
+
+// Set current date as default
+document.addEventListener("DOMContentLoaded", () => {
+    const dateInput = document.getElementById("date");
+    const today  = new Date().toISOString().split("T")[0];
+    dateInput.value = today;
+});
+
+// Save transaction handler
+document.getElementById("transactionForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+   
+    const type = document.getElementById("type").value;
+    const category = document.getElementById("category").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+    const date = document.getElementById("date").value;
+    const notes = document.getElementById("notes").value.trim();
+  
+    if (!type || !category || isNaN(amount) || amount <= 0 || !date) {
+      alert("Please fill in all required fields correctly.");
+      return;
+    }
+
+    const newTransaction = {
+        type,
+        category,
+        amount,
+        date,
+        notes
+    };
+
+
+    //  Load exist transactions or create new array
+    const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+    // add new transactions
+    transactions.push(newTransaction);
+
+
+    //  save back to localstorage
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    alert("Transaction saved successfully!");
+
+    // Redirect to dashboard
+    window.location.href = "index.html";
+  });
+  
+  // Back button
+  document.getElementById("backBtn").addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
